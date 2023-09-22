4d54c99722f818f044a2337c58076c91adbb68cd
anm11411422ubca057 <144470515+anm11411422ubca057@users.noreply.github.com>
2023-09-22 08:44:42 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,15 +1,24 @@
-class player:
-    def play (self):
-        print("The player is playing cricket.")
-class Batsman(player):
-    def play(self):
-        print("The batsman is bating.")
-class Bowler (player):
-    def play(self):
-        print("The bowler is bowling.")
-        
-batsman=Batsman()  
-bowler=Bowler() 
+#1.1 Implement o recursive function tocalculate the factorial of a given number
 
-batsman.play()
-bowler.play()
+"""
+1!=1×1
+2!=2 x 1! ---> 2 x 1 
+3!= 3 ×2! --->3 x 1
+.
+.
+10!=10 x 9! ---> 10 x 9 x... x 1
+
+Formula - n ×(n-1)!
+"""
+
+
+def fact_rec(n):
+      if n==0 or n==1:
+         return 1 
+      else:
+         return n*fact_rec(n-1)
+
+number = int(input("Enter a value :"))
+res = fact_rec (number)
+
+print("The factorial of {} is{}".format(number,res))
--- main (2).py
+++ main (2).py
@@ -1,24 +1,22 @@
+# Leap year
 """
-Write a function called linear_search_product that takes the list of products and a target product
-name as input. The function should perform a linear search to find the target product in the list and
-return a list of indices of all occurrences of the product if found, or an empty list if the product is not
-found.
+year % 4==0  &
+year% 100 != 0 /
+year %400 == 0
+
 """
 
 
-def linearSearchProduct(productList, targetProduct):
-  indices = []
-
-  for index, product in enumerate(productList):
-    if product == targetProduct:
-      indices.append(index)
-
-  return indices
+def tsLeapYear (year):
+  if (year % 4 ==0 and year %100 !=0)    or year%400==0:
+    return True
+  else: 
+    return False
 
 
-# Example usage:
-products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
-target = "shoes"
-target2 = 'apple'
-result = linearSearchProduct(products, target)
-print(result)
+year =int(input("Enter a year :"))
+
+if tsLeapYear(year):
+ print('{}is a leap year.'.format(year))
+else:
+  print('{} is not a leap year.'.format(year))
